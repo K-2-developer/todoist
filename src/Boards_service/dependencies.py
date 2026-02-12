1eeb3004d4205c04025323375a57668ea10216cb
Kovalyov <dev.kovalyov@gmail.com>
2026-02-12 10:35:50 +0300

    fixed board repo

--- src/Boards_service/dependencies.py
+++ src/Boards_service/dependencies.py
@@ -1,12 +1,16 @@
+from Boards_service.repository.implementations.BoardRepository import BoardRepository
 from src.Boards_service.service.workspace_service import WorkSpaceService
 from src.Boards_service.service.board_service import  BoardService
 from src.Boards_service.service.column_service import ColumnService
 from src.Boards_service.service.task_service import TaskService
+from src.Boards_service.Infrastructure.Database.database import get_session
 
 def get_workspace_service() -> WorkSpaceService:
     return WorkSpaceService(repo=None)
 
-def get_board_service() -> BoardService:
+def get_board_service( session: AsyncSession = Depends(get_session)) -> BoardService:
+    board_repo = BoardRepository(session)
+    workspace_repo = WorkSpaceRepository
     return BoardService(repo=None)
 
 def get_column_service() -> ColumnService:
--- src/Boards_service/service/board_service.py
+++ src/Boards_service/service/board_service.py
@@ -1,5 +1,7 @@
 from uuid import uuid4
 from typing import List
+
+from Boards_service.Infrastructure.Database.orm_models import board
 from src.Boards_service.repository.interfaces.board_interface import IBoardRepository
 from src.Boards_service.repository.interfaces.workspace_interface import IWorkSpaceRepository
 from src.Boards_service.schemas.board_schemas import *
@@ -19,25 +21,48 @@ class BoardService:
             title=data.title,
             description=data.description,
             workspace_id=data.workspace_id,
+            position=0,
+            is_archived=False
         )
         return await self.board.create(board)
 
     async def update_board(self, board_id: UUID, data: BoardUpdate) -> None:
-        if not await self.board.exists(board_id):
-            raise ValueError('Board not found')
-        return await self.board.update(board_id, data)
+        board = await self.board.get(board_id)
+        if data.title is not None:
+            board.title = data.title
+        if data.description is not None:
+            board.description = data.description
+        if data.is_archived is not None:
+            board.is_archived = data.is_archived
+        await self.board.update(board)
+
 
     async def delete_board(self, board_id: UUID) -> None:
-        if not await self.board.exists(board_id):
-            raise ValueError('Board not found')
         await self.board.delete(board_id)
 
-    async def get_board(self, board_id: UUID) -> BoardResponse:
-        if not await self.board.exists(board_id):
-            raise ValueError('Board not found')
-        return await self.board.get(board_id)
+
+    async def get_board(self, board_id: UUID) -> BoardResponse: #Проверка тут не нужна, т.к get сам выбросит ошибку
+        board = await self.board.get(board_id)                  #Не совсем чистая архитектура, т.к репозиторий видит Pydantic модели, но для понимания сделаю так.
+        return BoardResponse(                                   #Для чистой архитектуры бизнес логика в роутах?
+            id=board.id,
+            title=board.title,
+            description=board.description,
+            workspace_id=board.workspace_id,
+            position=board.position,
+            is_archived=board.is_archived,
+            created_at=board.created_at,
+            updated_at=board.updated_at,
+        )
+
+        '''async def get_board(self, board_id: UUID) -> Board:
+        return await self.board.get(board_id)   Вариант для чистой архитектуры, остальное в роуте прописать'''
+
 
     async def list_by_workspace(self, workspace_id: UUID) -> List[BoardNameResponse]:
         if not await self.workspace.exists(workspace_id):
             raise ValueError('Workspace not found')
-        return await self.board.list_by_workspace(workspace_id)
+        boards = await self.board.list_by_workspace(workspace_id)
+        return [BoardNameResponse(id=obj.id, title=obj.title) for obj in boards]
+
+
+#Если бизнес логика будет в роутах - тогда сервисы мало для чего нужны?
