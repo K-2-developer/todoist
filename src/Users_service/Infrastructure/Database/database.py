32006217706248041e8b91ce6687cac45d39ac51
Kovalyov <dev.kovalyov@gmail.com>
2026-03-26 15:56:20 +0300

    added docker container for user_app

--- src/Users_service/Infrastructure/Database/database.py
+++ src/Users_service/Infrastructure/Database/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+import os
 
-DATABASE_URL = "postgresql+asyncpg://postgres:12345@localhost:5432/User_service"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
